455f44bcd2ddbc86b653f3e5e0dee40433101c9a
thang <thang@LAPTOP-QVQD6BV9>
2024-07-06 13:34:49 -0400

    Third commit

---
+++ Corejavaprojects/src/com/tnsif/packagestwo/Display.java
@@ -0,0 +1,9 @@
+package com.tnsif.packagestwo;
+import com.tnsif.packages.*;
+public class Display {
+	public static void main(String[] args)
+	{
+		Publicdemo obj = new Publicdemo();
+		System.out.println("Different package "+obj.a);
+	}
+}
