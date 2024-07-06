78dccffa48c4579de615eef55360a14fe7a48936
thang <thang@LAPTOP-QVQD6BV9>
2024-07-06 12:27:42 -0400

    First commit

---
+++ Corejavaprojects/src/com/tnsif/encapsulation/Encapsulation_demo.java
@@ -0,0 +1,12 @@
+package com.tnsif.encapsulation;
+public class Encapsulation_demo 
+{
+ public static void main(String[] args) 
+ {
+	Encapsulationdemo a =new Encapsulationdemo();
+	a.name ="Deepthi";
+	a.serialnumber = 57;
+	a.age = 21;
+	a.show();
+}
+}
---
+++ Corejavaprojects/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Corejavaprojects {
+}
