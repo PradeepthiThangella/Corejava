b354cd96b6dd1d461d62cc5210cae61159634979
thang <thang@LAPTOP-QVQD6BV9>
2024-07-08 12:37:20 -0400

    sixth commit

---
+++ Corejavaprojects/src/com/tnsif/constructor/Customerdemo.java
@@ -0,0 +1,26 @@
+package com.tnsif.constructor;
+import java.util.Scanner;
+
+public class Customerdemo
+{
+public static void main(String[] args) 
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter your name");
+	String name=sc.nextLine();
+	System.out.println("enter your cid");
+	int id=sc.nextInt();
+	System.out.println("enter caddress");
+	String add=sc.next();
+	
+	Customer c=new Customer();
+	c.setCname(name);
+	c.setCid();
+	c.setCaddress(add);
+	
+	System.out.println(c);
+	boolean out= d instanceof Customer;
+	System.out.println(out);
+}
+
+}
---
+++ Corejavaprojects/src/com/tnsif/superkeyword/superex.java
@@ -0,0 +1,14 @@
+package com.tnsif.superkeyword;
+
+public class superex {
+int a=20;
+	
+	void drinking() {
+		System.out.println("coffe");
+	}
+	Superex() 
+	{
+		System.out.println("hello");
+	}
+
+}
