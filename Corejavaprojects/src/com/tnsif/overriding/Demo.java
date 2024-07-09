5a16ec1cfcb8de0c18eedcc1410a9a302bb01b22
thang <thang@LAPTOP-QVQD6BV9>
2024-07-09 01:18:44 -0400

    eight commit

---
+++ Corejavaprojects/src/com/tnsif/overriding/Demo.java
@@ -0,0 +1,10 @@
+package com.tnsif.overriding;
+
+public class Demo {
+  public static void main(String[] args) {
+	SBI s=new SBI();
+	s.getrateofinterest();
+	System.out.println(s.getrateofinterest());
+	
+}
+}
