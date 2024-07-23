d5ac5b7a923fb6fe908cf58b6db1ae4e2f2d4f71
thang <thang@LAPTOP-QVQD6BV9>
2024-07-23 11:31:54 -0400

    arraylist commit

---
+++ Corejavaprojects/src/com/tnsif/selfcode/Student.java
@@ -0,0 +1,16 @@
+package com.tnsif.selfcode;
+
+public class Student 
+{
+	 int rollno;
+	 String  name;
+	 char section;
+     public static void main(String[] args)
+      {
+	    Student s=new Student();
+	    s.rollno = 57;
+	    s.name = "Deepu";
+	    s.section ='C';
+	      System.out.println("rollno: "+rollno+ "\n name: "+name+ "\n section: "+section);
+      } 
+}
